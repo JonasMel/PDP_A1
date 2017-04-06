707f3bff544692ec19852c59d78a4dead4a2f744
JonasMel <jome@protonmail.com>
2017-04-06 12:45:41 +0200

    More communication stuff added

--- Assign1_1.c
+++ Assign1_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
 int main(int argc, char **argv)
 {
@@ -13,6 +14,7 @@ int main(int argc, char **argv)
 	int N, p, myid, ndim, rank, root, sqrt_p;
 	int dims[2], coords[2], cyclic[2], reorder;
 	MPI_Comm comm2D;
+	MPI_Datatype blk
 	p = atoi(argv[1]);
 	N = atoi(argv[2]);
 	MPI_Init(&argc, &argv);
@@ -25,6 +27,7 @@ int main(int argc, char **argv)
 	cyclic[1] = 0;
 	reorder = 0;
 	sqrt_p = sqrt(p);
+	blk_size = N/sqrt_p;
 	MPI_Barrier(MPI_COMM_WORLD);
 	dims[0] = sqrt_p;
 	dims[1] = sqrt_p;
@@ -56,7 +59,16 @@ int main(int argc, char **argv)
 	            B[i*N+j] = double(rand())/RAND_MAX;
 	        }
 	    }
-	  MPI_Type_vector
+	  MPI_Type_vector(blk_size,blk_size, N, MPI_DOUBLE, &blk);
+	  MPI_Type_commit(&blk);
+	  for (i = 0; i < sqrt_p; i++)
+	  {
+		  for (j = 0; j < sqrt_p; j++)
+		  {
+			  MPI_Cart_rank(comm2D, coords, &rank)
+			  MPI_Isend()
+		  }
+	  }
     }
     
 }
